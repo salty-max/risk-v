49dd1eaed0669e9aa8a275e8a29b96d768041359
salty-max <max@jellycat.fr>
2023-08-02 00:54:05 +0200

    chore: removes build from remote

--- build/module/pipeline/pipeline-stage.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PipelineStage = void 0;
-class PipelineStage {
-}
-exports.PipelineStage = PipelineStage;
--- build/module/util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toBinaryString = exports.toHexString = void 0;
-const toHexString = (n, l = 8) => `0x${n.toString(16).padStart(l, '0')}`;
-exports.toHexString = toHexString;
-const toBinaryString = (n, l = 32) => `0b${n.toString(2).padStart(l, '0')}`;
-exports.toBinaryString = toBinaryString;
